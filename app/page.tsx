774e4b79581842e450fd14ae2a8b79cd0fdb8053
tom <tom@pipefold.io>
2024-12-16 15:15:45 +0000

    wip: first pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
+"use client";
+import { useRef } from "react";
+import { VFXProvider, VFXSpan } from "react-vfx";
+
+const blink = `
+uniform vec2 resolution;
+uniform vec2 offset;
+uniform float time;
+uniform sampler2D src;
+// uniform vec2 changingValue; // ADD THIS LINE
+
+void main() {
+    vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+    gl_FragColor = texture2D(src, uv) * step(.5, fract(time));
+}
+`;
+
 const Home = () => {
+  const x = useRef(10);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Coming soon</h1>
-    </div>
+    <>
+      <VFXProvider>
+        <VFXSpan
+          shader={blink}
+          uniforms={{
+            changingValue: () => [x.current, x.current], //  VEC2
+          }}
+        >
+          I'm blinking!
+        </VFXSpan>
+      </VFXProvider>
+      <button
+        type="button"
+        onClick={() => {
+          x.current *= -1;
+        }}
+      >
+        CLICK
+      </button>
+    </>
   );
 };
 
