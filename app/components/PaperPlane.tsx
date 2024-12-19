b9ddb351f35ff21e0aa172259b1051161bd2f6f6
tom <tom@pipefold.io>
2024-12-19 11:22:24 +0000

    wip: getting there

--- app/components/PaperPlane.tsx
+++ app/components/PaperPlane.tsx
@@ -4,6 +4,11 @@ import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export function PaperPlane() {
+  // Dimensions that will be used for both plane and light
+  const width = 10;
+  const height = 10;
+  const lightOffset = 0.5; // Distance of light from the plane
+
   const [colorMap, normalMap, roughnessMap] = useTexture([
     "/assets/textures/Paper005/Paper005_1K-JPG_Color.jpg",
     "/assets/textures/Paper005/Paper005_1K-JPG_NormalGL.jpg",
@@ -16,17 +21,31 @@ export function PaperPlane() {
   });
 
   return (
-    <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
-      <planeGeometry args={[10, 10]} />
-      <meshStandardMaterial
-        color="#f5e6d3"
-        roughness={0.8}
-        metalness={0}
-        map={colorMap}
-        normalMap={normalMap}
-        roughnessMap={roughnessMap}
-        normalScale={[0.6, 0.6]}
+    <group>
+      {/* The paper plane */}
+      <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
+        <planeGeometry args={[width, height]} />
+        <meshStandardMaterial
+          color="#f5e6d3"
+          roughness={0.8}
+          metalness={0}
+          map={colorMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          normalScale={[0.6, 0.6]}
+        />
+      </mesh>
+
+      {/* Matching light */}
+      <rectAreaLight
+        position={[0, 0, lightOffset]}
+        width={width}
+        height={height}
+        intensity={1}
+        rotation={[0, 0, 0]}
       />
-    </mesh>
+
+      {/* <OrbitControls /> */}
+    </group>
   );
 }
--- app/components/R3F.tsx
+++ app/components/R3F.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 const R3F = ({ children }: { children: React.ReactNode }) => {
@@ -10,11 +9,6 @@ const R3F = ({ children }: { children: React.ReactNode }) => {
         alpha: false,
       }}
     >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 10, 5]} intensity={1} />
-      <directionalLight position={[-5, 5, -5]} intensity={0.5} />
-
-      <OrbitControls />
       {children}
     </Canvas>
   );
