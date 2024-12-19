0cf7c3d6e6f1b9ce8e82df750651a231fdfddbf9
tom <tom@pipefold.io>
2024-12-19 11:13:57 +0000

    wip: better

--- app/components/PaperPlane.tsx
+++ app/components/PaperPlane.tsx
@@ -10,23 +10,22 @@ export function PaperPlane() {
     "/assets/textures/Paper005/Paper005_1K-JPG_Roughness.jpg",
   ]);
 
-  // Make all textures tileable
   [colorMap, normalMap, roughnessMap].forEach((texture) => {
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(2, 2);
+    texture.repeat.set(1, 1);
   });
 
   return (
     <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <planeGeometry args={[10, 10]} />
       <meshStandardMaterial
-        color="#b5916f"
-        roughness={1}
+        color="#f5e6d3"
+        roughness={0.8}
         metalness={0}
         map={colorMap}
         normalMap={normalMap}
         roughnessMap={roughnessMap}
-        normalScale={[0.3, 0.3]}
+        normalScale={[0.6, 0.6]}
       />
     </mesh>
   );
