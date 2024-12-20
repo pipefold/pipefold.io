9693d66c382226416e6e2b0303233757767de4d4
tom <tom@pipefold.io>
2024-12-20 11:33:53 +0000

    wip: paper texture working pretty good

--- app/components/PaperPlane.tsx
+++ app/components/PaperPlane.tsx
@@ -1,42 +1,41 @@
 "use client";
 
-import { useTexture } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export function PaperPlane() {
-  // Dimensions that will be used for both plane and light
   const width = 10;
   const height = 10;
-  const lightOffset = 0.5; // Distance of light from the plane
+  const lightOffset = 0.5;
 
-  const [colorMap, normalMap, roughnessMap] = useTexture([
+  const [colorMap, normalMap, displacementMap] = useTexture([
     "/assets/textures/Paper005/Paper005_1K-JPG_Color.jpg",
     "/assets/textures/Paper005/Paper005_1K-JPG_NormalGL.jpg",
-    "/assets/textures/Paper005/Paper005_1K-JPG_Roughness.jpg",
+    "/assets/textures/Paper005/Paper005_1K-JPG_Displacement.jpg",
   ]);
 
-  [colorMap, normalMap, roughnessMap].forEach((texture) => {
+  [colorMap, normalMap, displacementMap].forEach((texture) => {
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(1, 1);
   });
 
   return (
     <group>
-      {/* The paper plane */}
       <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
-        <planeGeometry args={[width, height]} />
+        <planeGeometry args={[width, height, 256, 256]} />
         <meshStandardMaterial
           color="#f5e6d3"
-          roughness={0.8}
-          metalness={0}
+          // roughness={1}
+          // metalness={0}
           map={colorMap}
           normalMap={normalMap}
-          roughnessMap={roughnessMap}
-          normalScale={[0.6, 0.6]}
+          normalScale={[1.5, 1.5]}
+          displacementMap={displacementMap}
+          displacementScale={0.1}
+          displacementBias={-0.05}
         />
       </mesh>
 
-      {/* Matching light */}
       <rectAreaLight
         position={[0, 0, lightOffset]}
         width={width}
@@ -44,8 +43,7 @@ export function PaperPlane() {
         intensity={1}
         rotation={[0, 0, 0]}
       />
-
-      {/* <OrbitControls /> */}
+      <OrbitControls />
     </group>
   );
 }
